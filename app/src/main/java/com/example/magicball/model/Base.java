f6a9d20343d1ca6dd35dbebb5191d76cf72d7569
Brandon Caballero <60459450+BlokeAble@users.noreply.github.com>
2020-02-27 11:53:58 -0700

    MVC

---
+++ app/src/main/java/com/example/magicball/model/Base.java
@@ -0,0 +1,6 @@
+package com.example.magicball.model;
+
+public class Base
+{
+
+}
--- app/src/main/java/com/example/magicball/model/MagicObj.java
+++ app/src/main/java/com/example/magicball/model/MagicObj.java
@@ -1,4 +1,22 @@
 package com.example.magicball.model;
 
-public class MagicObj {
+import java.util.ArrayList;
+
+public class MagicObj
+{
+    private String name;
+    private ArrayList<String> good= new ArrayList<>(); // holds 10
+    private ArrayList<String> neutral= new ArrayList<>(); //holds 5
+    private ArrayList<String>  bad= new ArrayList<>(); //holds 5
+
+
+    public void createRandom()
+    {
+
+    }
+
+
+
+
+
 }
