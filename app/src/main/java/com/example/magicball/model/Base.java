3a674722945c0b194a81fef0c3b410eb0cefe956
Brandon Caballero <60459450+BlokeAble@users.noreply.github.com>
2020-02-28 12:01:22 -0700

    Arraylist Are being filled

--- app/src/main/java/com/example/magicball/model/Base.java
+++ app/src/main/java/com/example/magicball/model/Base.java
@@ -1,6 +1,79 @@
 package com.example.magicball.model;
 
-public class Base
+import java.util.ArrayList;
+
+public class Base extends MagicObj
 {
 
+    public Base()
+    {
+        setName("Magic Eight Ball");
+    }
+
+
+    /*
+     It is certain.
+● It is decidedly so.
+● Without a doubt.
+● Yes - definitely.
+● You may rely on it.
+● As I see it, yes.
+● Most likely.
+● Outlook good.
+● Yes.
+● Signs point to yes.
+     */
+
+    public void fillGood()
+    {
+        good.add("It is certain.");
+        good.add("It is decidedly so.");
+        good.add("Without a doubt");
+        good.add("Yes, definitely");
+        good.add("You may rely on it");
+        good.add("As I see it yes.");
+        good.add("Most likely");
+        good.add("Outlook good");
+        good.add("Yes");
+        good.add("Signs point to yes");
+    }
+
+    public void fillNeutral()
+    {
+        neutral.add("It is certain.");
+        neutral.add("It is decidedly so.");
+        neutral.add("Without a doubt");
+        neutral.add("Yes, definitely");
+        neutral.add("You may rely on it");
+    }
+
+    public void fillBad()
+    {
+        bad.add("It is certain.");
+        bad.add("It is decidedly so.");
+        bad.add("Without a doubt");
+        bad.add("Yes, definitely");
+        bad.add("You may rely on it");
+    }
+
+
+
+    @Override
+    public String toString()
+    {
+        return  super.toString();
+    }
 }
+
+
+/*
+Use for custom
+   public void Fill(ArrayList pick, int size,String input)
+    {
+        for(int i=0; i<size-1;i++)
+        {
+            pick.add(input);
+        }
+
+    }
+ */
--- app/src/main/java/com/example/magicball/model/MagicObj.java
+++ app/src/main/java/com/example/magicball/model/MagicObj.java
@@ -5,18 +5,38 @@ import java.util.ArrayList;
 public class MagicObj
 {
     private String name;
-    private ArrayList<String> good= new ArrayList<>(); // holds 10
-    private ArrayList<String> neutral= new ArrayList<>(); //holds 5
-    private ArrayList<String>  bad= new ArrayList<>(); //holds 5
+    protected  ArrayList<String> good= new ArrayList<>(); // holds 10
+    protected  ArrayList<String> neutral= new ArrayList<>(); //holds 5
+    protected  ArrayList<String>  bad= new ArrayList<>(); //holds 5
 
+
+    public void MagicObj(String name)
+    {
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public void createRandom()
     {
 
     }
 
+    public String toString()
+    {
+        return "You are using: "+name;
+    }
+
+
 
 
 
 
 }
+
