0f0868b0b3b9f2e69dec599dcdcd7214b5f879bc
hnevarez <61557047+hnevarez@users.noreply.github.com>
2020-02-28 12:18:25 -0700

    Changed the main menu to Just welcome to magic 8

--- app/src/main/java/com/example/magicball/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/magicball/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Welcome to Magic 8 Ball");
     }
 
     public LiveData<String> getText() {
